cb2ffb28e26b2c9a682c60bd1f8716bfca30af11
Sukharevich Vladislav <vladoslav.sukharevich3@gmail.com>
2021-03-11 11:04:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите E: ");
+            double eps = Convert.ToDouble(Console.ReadLine());
+            double S = 0;
+            double n = 1;
+            while (Math.Abs(-Math.Pow(1.0, n - 1.0) / Math.Pow(n, 2.0)) >= eps)
+            {
+                S += -Math.Pow(1.0, n - 1.0) / Math.Pow(n, 2.0);
+                n++;
+            }
+            Console.WriteLine($"Сумма ряда: {S}");
+        }
+    }
+}
